33c23942e459130c949dbdfdfab7a9bcbf466ebe
samsvr75 <samsvr75@gmail.com>
2021-03-28 20:19:14 +0200

    ajout logiciel et datasheet

---
+++ logiciel/src/renderer.js
@@ -0,0 +1,44 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+const serialport = require('serialport')
+const tableify = require('tableify')
+const Readline = require('@serialport/parser-readline');
+
+async function listSerialPorts() {
+  await serialport.list().then((ports, err) => {
+    if(err) {
+      document.getElementById('error').textContent = err.message
+      return
+    } else {
+      document.getElementById('error').textContent = ''
+    }
+    console.log('ports', ports);
+
+    if (ports.length === 0) {
+      document.getElementById('error').textContent = 'No ports discovered'
+    }
+
+    tableHTML = tableify(ports)
+    document.getElementById('ports').innerHTML = tableHTML
+  })
+}
+
+
+
+
+
+const port = new serialport('COM11', { baudRate: 9600 });
+const parser = port.pipe(new Readline({ delimiter: '\n' }));// Read the port data
+port.on("open", () => {
+  console.log('serial port open');
+});parser.on('data', data =>{
+  console.log('got word from arduino:', data);
+});
+// Set a timeout that will check for new serialPorts every 2 seconds.
+// This timeout reschedules itself.
+setTimeout(function listPorts() {
+  listSerialPorts();
+  setTimeout(listPorts, 2000);
+}, 2000);
